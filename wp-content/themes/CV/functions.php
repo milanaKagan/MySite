c9440ee9d38a4f1815d05e97f785657f80c9f7db
Milana Kagan <milana.l890@gmail.com>
2021-03-09 15:38:09 +0200

    scripts added

--- wp-content/themes/CV/functions.php
+++ wp-content/themes/CV/functions.php
@@ -1,8 +1,10 @@
 <?php
 
     function siteScripts() {
+        
+        wp_enqueue_script('scripts-js', get_template_directory_uri().'/js/scripts.js');
         wp_enqueue_style('style-css', get_template_directory_uri().'/style.css');
     }
+        
     add_action('wp_enqueue_scripts', 'siteScripts');
-
 ?>
